638e442523cd647dd603beb54262bd58ee651b14
Pete Browne <me@petebrowne.com>
2011-06-03 17:11:29 -0500

    Adding basic Tilt-like interface for Crush mappings

--- lib/crush.rb
+++ lib/crush.rb
@@ -1,2 +1,50 @@
 module Crush
+  extend self
+  
+  # Look for a compressor engine for the given filename or file
+  # extension. Return nil when no engine is found.
+  def [](path)
+    pattern = File.basename(path.to_s).downcase
+    until pattern.empty? || registered?(pattern)
+      pattern.sub!(/^[^.]*\.?/, "")
+    end
+    
+    pattern = normalize(pattern)
+    mappings[pattern].first
+  end
+  
+  # Hash of registered compression engines.
+  def mappings
+    @mappings ||= Hash.new { |h, k| h[k] = [] }
+  end
+  
+  # Ensures the extension doesn't include the "."
+  def normalize(ext)
+    ext.to_s.downcase.sub(/^\./, '')
+  end
+  
+  # Make the given compression engine preferred. Which means,
+  # it will reordered the beginning of its mapping.
+  def prefer(engine)
+    mappings.each do |ext, engines|
+      if engines.include?(engine)
+        engines.delete(engine)
+        engines.unshift(engine)
+      end
+    end
+  end
+  
+  # Registers a compression engine for the given file extension(s).
+  def register(engine, *extensions)
+    extensions.each do |ext|
+      ext = normalize(ext)
+      mappings[ext].unshift(engine).uniq!
+    end
+  end
+  
+  # Returns true when an engine exists for the given file extension.
+  def registered?(ext)
+    ext = normalize(ext)
+    mappings.key?(ext) && !mappings[ext].empty?
+  end
 end
---
+++ spec/crush_spec.rb
@@ -0,0 +1,78 @@
+require "spec_helper"
+
+describe Crush do
+  class MockCompressor
+    attr_reader :args, :block
+    def initialize(*args, &block)
+      @args = args
+      @block = block
+    end
+  end
+  
+  class MockCompressor2 < MockCompressor
+  end
+  
+  before(:all) do
+    Crush.register(MockCompressor,  "mock", ".mck", "file.txt")
+    Crush.register(MockCompressor,  "mult")
+    Crush.register(MockCompressor2, "mult")
+  end
+  
+  describe ".mappings" do
+    it "always returns an array" do
+      Crush.mappings["none"].should == []
+    end
+  end
+  
+  describe ".register" do
+    it "stores engines" do
+      Crush.mappings["mock"].should include(MockCompressor)
+      Crush.mappings["mck"].should include(MockCompressor)
+      Crush.mappings["file.txt"].should include(MockCompressor)
+    end
+    
+    it "prefers the latest registered engines" do
+      Crush.mappings["mult"].first.should == MockCompressor2
+    end
+  end
+  
+  describe ".registered?" do
+    it "returns false if the extension is not registered" do
+      Crush.registered?("none").should be_false
+    end
+    
+    it "returns true if the given extension is registered" do
+      Crush.registered?("mock").should be_true
+      Crush.registered?(".mock").should be_true
+    end
+  end
+  
+  describe ".[]" do
+    it "returns nil when no engines are found" do
+      Crush["none"].should be_nil
+    end
+    
+    it "returns engines matching exact extension names" do
+      Crush["mock"].should == MockCompressor
+    end
+    
+    it "returns engines matching exact file extensions" do
+      Crush[".MOCK"].should == MockCompressor
+    end
+    
+    it "returns engines matching multiple file extensions" do
+      Crush["application.js.Mock"].should == MockCompressor
+    end
+    
+    it "returns engines matching filenames" do
+      Crush["some/path/file.txt"].should == MockCompressor
+    end
+  end
+  
+  describe ".prefer" do
+    it "reorders the engine mappings so the given engine is returned" do
+      Crush.prefer(MockCompressor)
+      Crush["mult"].should == MockCompressor
+    end
+  end
+end
