7e68c05792664a94fa9408750fbee17f3b97b81e
Pete Browne <me@petebrowne.com>
2011-08-27 15:10:48 -0500

    Adding registration methods

--- lib/crush.rb
+++ lib/crush.rb
@@ -1,6 +1,8 @@
 require "crush/version"
 
 module Crush
+  extend self
+  
   autoload :Closure,   "crush/closure"
   autoload :CSSMin,    "crush/cssmin"
   autoload :Engine,    "crush/engine"
@@ -9,4 +11,43 @@ module Crush
   autoload :Rainpress, "crush/rainpress"
   autoload :Uglifier,  "crush/uglifier"
   autoload :YUI,       "crush/yui"
+  
+  # Registers all of the JavaScripts engines
+  # with Tilt in the following order of importance:
+  #
+  # 1. Crush::Uglifer
+  # 2. Crush::Closure::Compiler
+  # 3. Crush::YUI::JavaScriptCompressor
+  # 4. Crush::Packr
+  # 5. Crush::JSMin
+  def register_js
+    Tilt.register JSMin,                     "js"
+    Tilt.register Packr,                     "js"
+    Tilt.register YUI::JavaScriptCompressor, "js"
+    Tilt.register Closure::Compiler,         "js"
+    Tilt.register Uglifier,                  "js"
+  end
+  
+  # Registers all of the CSS engines
+  # with Tilt in the following order of importance:
+  #
+  # 3. Crush::CSSMin
+  # 4. Crush::Rainpress
+  # 5. Crush::YUI::CssCompressor
+  def register_css
+    Tilt.register CSSMin,             "css"
+    Tilt.register Rainpress,          "css"
+    Tilt.register YUI::CssCompressor, "css"
+  end
+  
+  # Registers all of the included engines
+  # with Tilt.
+  #
+  # (see #register_js)
+  # (see #register_css)
+  def register
+    register_js
+    register_css
+  end
+  alias :register_all :register
 end
---
+++ spec/crush_spec.rb
@@ -0,0 +1,49 @@
+require "spec_helper"
+
+describe Crush do
+  describe ".register_js" do
+    it "registers all of the JavaScript engines with Tilt" do
+      Tilt.mappings.delete "js"
+      Crush.register_js
+      Tilt.mappings["js"].should == [
+        Crush::Uglifier,
+        Crush::Closure::Compiler,
+        Crush::YUI::JavaScriptCompressor,
+        Crush::Packr,
+        Crush::JSMin
+      ]
+    end
+  end
+  
+  describe ".register_css" do
+    it "registers all of the CSS engines with Tilt" do
+      Tilt.mappings.delete "css"
+      Crush.register_css
+      Tilt.mappings["css"].should == [
+        Crush::YUI::CssCompressor,
+        Crush::Rainpress,
+        Crush::CSSMin
+      ]
+    end
+  end
+  
+  describe ".register" do
+    it "registers all of the engines with Tilt" do
+      Tilt.mappings.delete "js"
+      Tilt.mappings.delete "css"
+      Crush.register
+      Tilt.mappings["js"].should == [
+        Crush::Uglifier,
+        Crush::Closure::Compiler,
+        Crush::YUI::JavaScriptCompressor,
+        Crush::Packr,
+        Crush::JSMin
+      ]
+      Tilt.mappings["css"].should == [
+        Crush::YUI::CssCompressor,
+        Crush::Rainpress,
+        Crush::CSSMin
+      ]
+    end
+  end
+end
